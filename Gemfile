4392d951c683c7119cf60615c8ec89632ab83649
Doug Letwin <dletwin7506@gmail.com>
2018-03-19 13:48:56 -0400

    first commit

---
+++ Gemfile
@@ -0,0 +1,22 @@
+source 'https://rubygems.org'
+ruby '2.2.2'
+
+gem 'rails', '4.0.13'
+gem 'sqlite3'
+gem 'sass-rails', '~> 4.0.2'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder', '~> 1.2'
+gem 'carrierwave'
+gem 'mini_magick'
+
+group :development, :test do
+  gem 'pry'
+end
+
+group :devlopment do
+  gem 'web-console', '~> 2.0'
+  gem 'spring'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Recipes::Application.config.secret_key_base = '1c38199af965def5d9d48b5c42b7f9f612b3e81c1eb6d07821599b74aac0a045426419853d9c17c0a304a90cc7e27f829ba22d62c3f284edc5ade491598bb98d'
