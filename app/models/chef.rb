81ba27de603938caa9c991b3f093c0cb0923ff1c
Doug Letwin <dletwin7506@gmail.com>
2018-03-20 12:23:49 -0400

    updated dependencies

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -3,5 +3,5 @@ class Chef < ActiveRecord::Base
   before_save { self.email = email.downcase }
   validates :chefname, presence: true, length: { minimum: 3, maximum: 40 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, lenth: { maximum: 105 }, uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX }
+  validates :email, presence: true, length: { maximum: 105 }, uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX }
 end
